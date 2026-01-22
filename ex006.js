9244aa9da5c7ed886138b7f7ce7d6e9fae022a7b
Juliana Eiyng <jueiyng@icloud.com>
2026-01-21 21:59:06 -0300

    Add files via upload

---
+++ ex006.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(` A velocidade do carro é ${vel}km/h`)
+if (vel>60){ // Condições simples!
+console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usanso cinto de segurança!`)
+
